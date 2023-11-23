ee4ff8ca5d331f6a8560e1f44bb6ba27c3beeff4
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2023-11-23 19:12:43 +0530

    Update Student.cs
    added comment to stdunt.cs

--- Student.cs
+++ Student.cs
@@ -1,9 +1,11 @@
-﻿namespace WebAPI_2
+namespace WebAPI_2
 {
-    public class Student
-    {
-        public int Id { get; set; }
+  public class Student // Defines a class named 'Student'
+  {
+    public int Id { get; set; } // Represents the student's unique identifier
+      // This property allows accessing and modifying the student's ID
 
-        public string Name { get; set; }
-    }
+    public string Name { get; set; } // Represents the student's name
+      // This property allows accessing and modifying the student's name
+  }
 }
